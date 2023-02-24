d938fe9706ae5889178ec20c6a3b289481bdd0ca
ivanH <nigga.2023@mail.ru>
2023-02-24 07:24:05 +0300

    zadacha 2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+﻿sum = int(input("enter  sum number: "))
+mult = int(input("enter mult number: "))
+
+for i in range(sum):
+    for j in range(mult):
+        if sum == i + j and mult == i * j:
+            print(f"numbers are: {i}, {j}")
+
+
+
